47970589c616d191495353ff89f5646b1a128744
Benjamin <benseed.bs@gmail.com>
2022-08-13 12:45:11 +0100

    adapted the 'pet' function to include hunger and fitness

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -25,3 +25,25 @@ describe('growUp', () => {
         expect(pet.age).toEqual(1);
     });
 });
+
+describe('growUp', () => {
+    it('increase age by 1', () => {
+        const pet = new Pet('Pippy')
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+    });
+});
+
+describe('growUp', () => {
+    it('when age invreases by 1, increase hunger by 5, decrease fitness by 3', () => {
+        const pet = new Pet('Pippy')
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+        expect(pet.hunger).toEqual(5);
+        expect(pet.fitness).toEqual(7);
+    });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,17 @@
 function Pet(name, age) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
 // this.name = name, this refers to the object returned from
 // by calling new Pet(), so this.name refers to the name property of that object
 
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
 };
+
 
 module.exports = Pet;
