5418eb6689f78d1a850d527e8b6a9d1eb170f3a0
Benjamin <benseed.bs@gmail.com>
2022-08-13 11:41:53 +0100

    added prototype method

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -2,6 +2,26 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-        expect(new Pet('Fido')).toBeInstanceOf(Object);
+        expect(new Pet('Pippy')).toBeInstanceOf(Object);
+    });
+    it('sets the name property', () => {
+        const pet = new Pet('Pippy');
+
+        expect(pet.name).toEqual('Pippy');
+    });
+    it('it has an initial age of 0', () => {
+        const pet = new Pet('Pippy');
+
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    it('increments the age by 1', () => {
+        const pet = new Pet('Pippy')
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,12 @@
-function Pet() {}
+function Pet(name, age) {
+    this.name = name;
+    this.age = 0;
+}
+// this.name = name, this refers to the object returned from
+// by calling new Pet(), so this.name refers to the name property of that object
+
+Pet.prototype.growUp = function() {
+    this.age += 1;
+};
 
 module.exports = Pet;
